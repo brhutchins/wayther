e0bd184dad226b1fd846e77e7ed361ef1a1d166a
brhutchins <bhutchins@gmail.com>
2021-10-02 22:13:09 +0200

    Removed errors packages

--- default.nix
+++ default.nix
@@ -1,18 +1,20 @@
-{ mkDerivation, aeson, base, bytestring, containers, errors
+{ mkDerivation, aeson, base, bytestring, containers, hpack
 , http-conduit, lib, mr-env, text
 }:
 mkDerivation {
   pname = "wayther";
-  version = "0.1.0.0";
+  version = "0.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson base bytestring containers errors http-conduit mr-env text
+    aeson base bytestring containers http-conduit mr-env text
   ];
+  libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
-    aeson base bytestring containers errors http-conduit mr-env text
+    aeson base bytestring containers http-conduit mr-env text
   ];
+  prePatch = "hpack";
   license = "unknown";
   hydraPlatforms = lib.platforms.none;
 }
